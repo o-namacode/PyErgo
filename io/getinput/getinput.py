0f2df7f521fc73b1396940fb3227907dd0496f48
Rápheekwon Rhooms <rapheekwon@gmail.com>
2024-09-06 10:35:12 -0500

    Update getinput.py

--- io/getinput/getinput.py
+++ io/getinput/getinput.py
@@ -1,2 +1,38 @@
-def getinput():
-    pass
+from typing import Optional
+
+from ..constants import IO__DEFAULT_MAX_WIDTH, IO__LEFT_PAD, NLBR
+from ..helper import get_left_pad_str
+
+
+def getinput(
+        prompt: Optional[str] = None,
+        left_pad: Optional[int] = IO__LEFT_PAD,
+        cursor: Optional[str] = None,
+        nl: bool = True,
+
+        strip: bool = True,
+        lower: bool = False,
+):
+    if cursor is None:
+        cursor = '> '
+    cursor_str = cursor + "  "
+
+    _prompt = get_left_pad_str(left_pad) + prompt
+    if nl:
+        _prompt += NLBR
+    _prompt += cursor_str if not nl else get_left_pad_str(left_pad) + cursor_str
+
+    if nl:
+        _prompt += NLBR
+
+    ui = input(_prompt)
+
+    if not ui:
+        return None
+    if strip:
+        ui = ui.strip()
+    if lower:
+        ui = ui.lower()
+
+    return ui
+
