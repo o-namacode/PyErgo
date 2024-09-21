ccbf7c3e8da838ee5fbb3b64ed5515367eced9da
Rápheekwon Rhooms <rapheekwon@gmail.com>
2024-09-21 10:05:03 -0500

    Update __init__.py

--- src/pyergo/interfaces/__init__.py
+++ src/pyergo/interfaces/__init__.py
@@ -4,3 +4,5 @@ from .iPasswordManager import IPasswordManager
 from .iApplication import IApplication
 
 from .iUserAccount import IUserAccount
+from .iUserManager import IUserManager
+from .iUserRole import IUserRole
