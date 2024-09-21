e3d789efecaff0e54e483b0a8c4cc75817a10e3c
Rápheekwon Rhooms <rapheekwon@gmail.com>
2024-09-21 09:59:29 -0500

    Update rolemanager.py

--- src/pyergo/managers/rolemanager.py
+++ src/pyergo/managers/rolemanager.py
@@ -1,12 +1,31 @@
 
-from typing import Generic, TypeVar
+from typing import Generic, List, TypeVar, Union
 
-from ..interfaces import iUserRole, iUserAccount
+from ..interfaces import iUserRole, iUserAccount, iUserManager
 
 
 TUser = TypeVar("TUser", bound=iUserAccount)
 TUserRole = TypeVar("TUserRole", bound=iUserRole)
+TUserManager = TypeVar("TUserManager", bound=iUserManager)
 
 
-class RoleManager (Generic[TUserRole]):
-    pass
+class RoleManager (Generic[TUser, TUserRole, TUserManager]):
+    @classmethod
+    def Add (cls, user : TUser, role_or_roles : Union[TUserRole, List[TUserRole]]):
+        if not hasattr (user, 'roles'):
+            raise AttributeError("Attribute `roles` not found in user implementation.")
+        
+        current_roles = user.roles
+
+        def _add_role(role : TUserRole):
+            if role in user.roles:
+                return
+            
+            user.roles.append(role)
+
+        if isinstance(role_or_roles, List[TUserRole]):
+            for rta in role_or_roles:
+                _add_role(rta)
+        elif isinstance(role_or_roles, TUserRole):
+            _add_role(role_or_roles)
+
