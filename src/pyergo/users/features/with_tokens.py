af2e54ad1e165430d47049e53399d63409f5175b
Rápheekwon Rhooms <rapheekwon@gmail.com>
2024-09-21 23:04:20 -0500

    Changes to be committed: 	new file:   src/pyergo/users/features/with_tokens.py

---
+++ src/pyergo/users/features/with_tokens.py
@@ -0,0 +1,36 @@
+from typing import Final, Optional
+from uuid import UUID
+
+from ...interfaces import IUserAccount
+
+class UserWithTokens (IUserAccount):
+    TOKEN_PREFIX : Final[str] = 'tk_'
+
+    def __init__(
+            self, 
+            id: str | UUID | None = None, 
+            
+            ef__raise_error_on_key_not_found: bool = False, 
+            *args, **kwargs):
+        
+        super().__init__(id, ef__raise_error_on_key_not_found, *args, **kwargs)
+        
+        # Properties 
+        self.tk_remember : Optional[str] = kwargs.get('tk_remember', None)
+
+    
+    def token(self, key : str, default = None):
+        aname = f"{self.TOKEN_PREFIX}{key}".lower()
+
+        if hasattr(self, aname):
+            return getattr(self, aname)
+        
+        if default is None:
+            raise AttributeError(f"Token Attribute `{key}` not found.")
+        
+        return default
+
+    def settoken(self, key : str, value):
+        tname = f"{self.TOKEN_PREFIX}{key}".lower()
+        setattr(self, tname, value)
+        return self
