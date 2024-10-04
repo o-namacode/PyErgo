4e7e69410f653f993d7a7411bfebfda08050a76a
Rápheekwon Rhooms <rapheekwon@gmail.com>
2024-10-03 23:43:31 -0500

    Update constants.py

--- src/pyergo/constants.py
+++ src/pyergo/constants.py
@@ -3,6 +3,9 @@ from .fileutils import safedir
 
 
 PYERGO__LIBRARY_BASEDIR = safedir(os.path.join(os.path.expanduser("~"), ".pyergo"))
+def pyergo_lib__project_basedir(project_dir : str):
+    return safedir(os.path.join(os.path.expanduser("~"), project_dir))
+
 
 PYERGO__LIBRARY_USERSFILE_DIR = safedir(os.path.join(PYERGO__LIBRARY_BASEDIR, "users"))
 def pyergo_lib__usersfile_dir(project_dir : str):
