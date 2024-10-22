dec91476222ed79983bdd8a59c96379e780b157f
Rápheekwon Rhooms <rapheekwon@gmail.com>
2024-10-21 21:17:32 -0500

    Update err_commmand_not_found.py

--- src/pyergo/exceptions/err_commmand_not_found.py
+++ src/pyergo/exceptions/err_commmand_not_found.py
@@ -1,3 +1,3 @@
 class CommandNotFoundError(Exception):
-    def __init__(self, command : str, *args: object) -> None:
-        super().__init__(f"Command `{command}` Not Found.")
+    def __init__(self, command : str, *args) -> None:
+        super().__init__(f"Command `{command}` Not Found.", *args)
