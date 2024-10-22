c6e04252eada8a16a8aa3deb93528fafb113220b
Rápheekwon Rhooms <rapheekwon@gmail.com>
2024-10-22 01:43:57 -0500

    Update __init__.py

--- src/pyergo/ioutils/output/__init__.py
+++ src/pyergo/ioutils/output/__init__.py
@@ -3,3 +3,4 @@ from .printborder import PrintBorder
 from .printerror import PrintError
 from .printwithborder import PrintWithBorder
 from .printlines import PrintLines
+from .printheader import PrintHeader
